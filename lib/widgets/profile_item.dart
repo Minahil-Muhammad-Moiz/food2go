1e7630aa0440b79a6863f25ed840f96fe7514c01
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2026-08-13 16:51:23 +0500

    profile

--- lib/widgets/profile_item.dart
+++ lib/widgets/profile_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:get/get.dart';
 
 class ProfileItem extends StatelessWidget {
   final IconData icon;
