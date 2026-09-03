5df0fcec5f4ea275e88ec5cd2d6100f403e68b68
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2026-09-03 07:27:10 +0500

    package compatibility issues

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,19 +6,17 @@ plugins {
 
 android {
     namespace = "com.developer.food2go"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.developer.food2go"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -27,8 +25,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -42,4 +38,9 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+// ADD THIS ENTIRE BLOCK AT THE VERY BOTTOM OF YOUR FILE
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,10 +15,29 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+// Safely override SDK levels regardless of current project evaluation state
+subprojects {
+    val configureAction = Action<Project> {
+        if (extensions.findByName("android") != null) {
+            configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(36)
+            }
+        }
+    }
+
+    // If already evaluated, execute now. Otherwise, hook into afterEvaluate.
+    if (state.executed) {
+        configureAction.execute(this)
+    } else {
+        afterEvaluate(configureAction)
+    }
 }
